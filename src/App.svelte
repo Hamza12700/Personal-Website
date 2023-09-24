285a48925d2e04596bd502bf792b688148bd0bd3
Hamza <72913359+Hamza12700@users.noreply.github.com>
2023-09-24 05:16:50 -0700

    Made it responsive

--- src/App.svelte
+++ src/App.svelte
@@ -3,9 +3,11 @@
 </script>
 
 <NavBar />
-<main class="mt-36 mx-10">
-  <h1 class="text-7xl mb-3 font-firaCodeBold">Hey, I'm Hamza</h1>
-  <h2 class="text-sky-500 text-4xl font-notoSans_Bold">Front-End Developer</h2>
+<main class="mt-32 md:mt-36 mx-5 md:mx-10">
+  <h1 class="text-5xl md:text-7xl mb-3 font-firaCodeBold">Hey, I'm Hamza</h1>
+  <h2 class="text-sky-500 text-3xl md:text-4xl font-notoSans_Bold">
+    Front-End Developer
+  </h2>
   <p class="my-4 md:w-[45rem] leading-8 font-poppinsMed text-xl">
     I'm passionate about the dynamic world of web development, with a particular
     focus on front-end technologies. In this ever-evolving field, I find myself
@@ -15,16 +17,18 @@
     bring creativity and innovation to every project I undertake while enjoying
     the thrill of coding along the way.
   </p>
-  <button class="btn mr-3" type="button">Projects</button>
+  <button class="btn w-full sm:w-auto mb-5 sm:mr-3" type="button"
+    >Projects</button
+  >
   <a
-    class="transition mx-2 ease-in-out hover:text-sky-500"
+    class="transition hidden sm:inline mx-2 ease-in-out hover:text-sky-500"
     target="_blank"
     href="https://github.com/hamza12700/"
   >
     <i class="fa-brands mx-1 fa-github-alt fa-xl" />
   </a>
   <a
-    class="transition mx-2 ease-in-out hover:text-sky-500"
+    class="transition hidden sm:inline mx-2 ease-in-out hover:text-sky-500"
     target="_blank"
     href="https://x.com/Hamza_Rash1d"
   >
