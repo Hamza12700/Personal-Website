66021481c9510fb52a0415996c48bc9a033f8391
Hamza <72913359+Hamza12700@users.noreply.github.com>
2023-09-24 04:25:03 -0700

    Importing `NavBar` component

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,5 @@
-<nav class="flex justify-between p-4 mt-2">
-  <h1 class="font-dancingScript text-3xl">Hamza Rashid</h1>
-  <section class="flex font-poppins items-center gap-3">
-    <a
-      class="transition hover:-translate-y-1 mx-2 ease-in-out hover:text-sky-500"
-      target="_blank"
-      href="https://github.com/hamza12700/"
-    >
-      <i class="pr-1 fa-brands fa-github-alt fa-xl" />
-      <span>GitHub</span>
-    </a>
+<script lang="ts">
+  import NavBar from "./lib/NavBar.svelte";
+</script>
 
-    <a
-      class="transition hover:-translate-y-1 mx-2 ease-in-out hover:text-sky-500"
-      target="_blank"
-      href="https://x.com/Hamza_Rash1d"
-    >
-      <i class="fa-brands fa-x-twitter fa-xl" />
-      <span>Twitter</span>
-    </a>
-
-    <button
-      class="ml-4 text-sky-500 transition ease-in-out hover:-translate-y-1 hover:bg-sky-500 hover:text-white border-2 border-sky-500 rounded-full py-2 px-4 font-bold"
-      type="button"
-      ><a href="https://github.com/Hamza12700?tab=repositories" target="_blank">
-        Projects
-      </a>
-    </button>
-  </section>
-</nav>
+<NavBar />
