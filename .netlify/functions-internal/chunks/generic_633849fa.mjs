ebe20bbace7ae6134891421ef6f7b65f5cdb1bed
Hamza <72913359+Hamza12700@users.noreply.github.com>
2023-10-16 08:24:07 -0700

    added `.netlify` directory

---
+++ .netlify/functions-internal/chunks/generic_633849fa.mjs
@@ -0,0 +1,6 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+
+const page = () => import('./pages/generic_63f4ac33.mjs').then(n => n.g);
+
+export { page };
