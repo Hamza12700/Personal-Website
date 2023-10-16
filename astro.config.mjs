0ade32052af9f635371df29410f04ae2132aedd1
Hamza <72913359+Hamza12700@users.noreply.github.com>
2023-10-16 08:43:12 -0700

    remove `netlify` adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,7 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from 'astro/config';
 
-import netlify from "@astrojs/netlify/functions";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  server: {
-    headers: {
-      "Cache-Control": "public, max-age=31536000"
-    }
-  },
-  output: "hybrid",
-  adapter: netlify()
 });
