e19870a61c81d2c933d8247be2ef2fd2ffb1b67e
Hamza <72913359+Hamza12700@users.noreply.github.com>
2023-10-16 06:31:58 -0700

    added `@astrojs/netlify`

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
+import tailwind from "@astrojs/tailwind";
 import { defineConfig } from 'astro/config';
 
-import tailwind from "@astrojs/tailwind";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  server: {
+    headers: {
+      "Cache-Control": "public, max-age=31536000"
+    }
+  },
+  output: "hybrid",
+  adapter: netlify()
 });
