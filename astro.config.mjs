14871624fb77e2b40e8a6ad0d5852af236c721aa
Hamza <72913359+Hamza12700@users.noreply.github.com>
2023-12-09 13:25:42 +0000

    enabled `speed-insights`

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 import tailwind from "@astrojs/tailwind";
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  output: "static",
+  adapter: vercel({
+    speedInsights: { enabled: true },
+  }),
 });
