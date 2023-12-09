2f00837380201cd6a390149d49a83fe78a539954
Hamza <72913359+Hamza12700@users.noreply.github.com>
2023-12-09 14:04:25 +0000

    remove `@astrojs/vercel` adpatar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,7 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
-import vercel from "@astrojs/vercel/static";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "static",
-  adapter: vercel({
-    speedInsights: { enabled: true },
-  }),
 });
