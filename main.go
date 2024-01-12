9002c4ab394def25d0f2ce2cc72c72da006bff6a
Hamza <72913359+Hamza12700@users.noreply.github.com>
2024-01-12 20:10:07 +0400

    move: `No-Build` directory to main

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"text/template"
+
+	"github.com/labstack/echo/v4"
+)
+
+type TemplateRegistry struct {
+	tamplates *template.Template
+}
+
+func (t *TemplateRegistry) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.tamplates.ExecuteTemplate(w, name, data)
+}
+
+func indexPage(c echo.Context) error {
+	return c.Render(http.StatusOK, "layout.html", nil)
+}
+
+func main() {
+	e := echo.New()
+
+	e.Renderer = &TemplateRegistry{
+		tamplates: template.Must(template.ParseGlob("templates/*.html")),
+	}
+
+	e.Static("/public", "dist")
+
+	e.GET("/", indexPage)
+
+	e.Logger.Fatal(e.Start(":1111"))
+}
