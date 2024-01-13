0c37b47bb6b42832c8e55435bf112f638e08c3c1
Hamza <72913359+Hamza12700@users.noreply.github.com>
2024-01-13 22:19:25 +0400

    added: function to ouput the generated html in `dist/index.html`

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"io"
 	"net/http"
+	"os"
+	"os/exec"
 	"text/template"
 
 	"github.com/labstack/echo/v4"
@@ -21,6 +23,8 @@ func indexPage(c echo.Context) error {
 }
 
 func main() {
+	build()
+
 	e := echo.New()
 
 	e.Renderer = &TemplateRegistry{
@@ -32,4 +36,28 @@ func main() {
 	e.GET("/", indexPage)
 
 	e.Logger.Fatal(e.Start(":1212"))
+}
+
+func build() {
+	if len(os.Args) != 2 {
+		return
+	}
+	arg := os.Args[1]
+	
+	if arg == "prod" {
+
+		tailwind_Minified := exec.Command("tailwind -o static/output.css --minify")
+		tailwind_Minified.Stderr = os.Stderr
+
+		templ := template.Must(template.ParseGlob("templates/*.html"))
+
+		tailwind_Minified.Run()
+
+		outputHTML, _ := os.Create("dist/index.html")
+		defer outputHTML.Close()
+
+		templ.ExecuteTemplate(outputHTML, "layout.html", nil)
+	}
+
+	os.Exit(0)
 }
